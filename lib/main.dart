339316c047bbc8e62509e12f288117cdc2f0ee93
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-10-19 23:09:58 +0300

    add strat

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter/services.dart';
-
-/*
-  قائمة المهام المستقبلية للتطبيق:
-  1- إضافة app bar مع Drawer في الجهة اليمنى
-  2- إضافة أيقونة التصفية (Filter)
-  3- إضافة ميزة Hot Reload (ميزة بيئة التطوير)
-  4- إضافة الوضع المظلم (Dark Mode)
-  5- ربط التطبيق مع API
-  6- إضافة تشفير للبيانات
-  7- تحسين الوصولية (Accessibility)
-  8- إضافة أزرار التفاعل (إعجاب، مشاركة، حفظ)
-  9- إضافة زر لعرض حسابات المشروع
-  10- تحسين التنقل بين الصفحات
-*/
 
 void main() {
-  runApp(const AwallimnaApp());
-}
-
-class AwallimnaApp extends StatelessWidget {
-  const AwallimnaApp({super.key});
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  ));
 }
